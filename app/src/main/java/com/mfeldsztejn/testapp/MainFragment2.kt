175d7f06f0af24492ff91bb18c3c26de2bee8b9e
Martin Feldsztejn <mfeldsztejn@gmail.com>
2020-03-17 12:49:14 -0300

    Testing native and custom FileProvider

--- app/src/main/java/com/mfeldsztejn/testapp/MainFragment2.kt
+++ app/src/main/java/com/mfeldsztejn/testapp/MainFragment2.kt
@@ -2,7 +2,6 @@ package com.mfeldsztejn.testapp
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.main_fragment.*
@@ -13,6 +12,8 @@ class MainFragment2 : Fragment(R.layout.main_fragment) {
         super.onViewCreated(view, savedInstanceState)
         message.text = "$this"
         goto2.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment2_to_mainFragment3))
-        goto3.isVisible = false
+        goto3.setOnClickListener {
+            shareViewAsScreenshot(requireContext(), view, "test1", false)
+        }
     }
 }
